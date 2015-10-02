da6931487f6b157beda405c687523a2fcead7333
Jesse Rohland <jesse.rohland@gmail.com>
2015-10-01 20:53:28 -0400

    Cleaning up TimePrecision enum

--- src/main/scala/org/influxdb/enums/TimePrecision.scala
+++ src/main/scala/org/influxdb/enums/TimePrecision.scala
@@ -2,5 +2,10 @@ package org.influxdb.enums
 
 object TimePrecision extends Enumeration {
   type TimePrecision = Value
-  val h, m, s, ms, u, ns = Value
+  val HOUR = Value("h")
+  val MINUTE = Value("m")
+  val SECOND = Value("s")
+  val MILLISECOND = Value("ms")
+  val MICROSECONDS = Value("u")
+  val NANOSECONDS = Value("ns")
 }
