bb24bed3560caf0eeed6c5205587db09fc1ad49c
Jesse Rohland <jesse.rohland@gmail.com>
2015-10-02 11:17:37 -0400

    Creating response series map helper method

--- src/main/scala/org/influxdb/response/Series.scala
+++ src/main/scala/org/influxdb/response/Series.scala
@@ -1,3 +1,15 @@
 package org.influxdb.response
 
-case class Series(columns: List[String], name: Option[String], values: List[List[Any]])
+case class Series(columns: List[String], name: Option[String], values: List[List[Any]]) {
+
+  def toSeriesMap: List[Map[String, Any]] = {
+    values.map(valueArray => {
+      var i = -1
+      valueArray.map(value => {
+        i += 1
+        (columns(i), value)
+      }).toMap
+    })
+  }
+
+}
