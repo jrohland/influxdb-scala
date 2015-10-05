c51e34931edb76b1d0d8052cfb40ccad9d8b2a46
Jesse Rohland <jesse.rohland@gmail.com>
2015-10-05 13:52:19 -0400

    Null handling in series

--- src/main/scala/org/influxdb/Series.scala
+++ src/main/scala/org/influxdb/Series.scala
@@ -20,25 +20,44 @@ case class Series(name: String,
   numberFormat.setMinimumFractionDigits(1)
 
   def keysStr: String = {
+    if (name == null) {
+      throw new NullPointerException("Null series name")
+    }
+
     val seriesName = s"${keyEscaper.escape(name)}"
     val keysStr = keys.map(key => {
-      s"${keyEscaper.escape(key._1)}=${keyEscaper.escape(key._2)}"
+      if (key._1 != null) {
+        if (key._2 != null) {
+          s"${keyEscaper.escape(key._1)}=${keyEscaper.escape(key._2)}"
+        } else {
+          throw new NullPointerException(s"Null key value for key: ${key._1}")
+        }
+      } else {
+        throw new NullPointerException("Null key name")
+      }
     }).mkString(",")
     s"$seriesName,$keysStr"
   }
 
   def fieldsStr: String = {
     fields.map(field => {
-      s"${fieldEscaper.escape(field._1)}=" + (
-        field._2 match {
-          case s: String =>
-            "\"" + fieldEscaper.escape(s) + "\""
-          case n: java.lang.Number =>
-            s"${numberFormat.format(n)}"
-          case default =>
-            s"$default"
+      if (field._1 != null) {
+        if (field._2 != null) {
+          s"${fieldEscaper.escape(field._1)}=" + (
+            field._2 match {
+              case s: String =>
+                "\"" + fieldEscaper.escape(s) + "\""
+              case n: java.lang.Number =>
+                s"${numberFormat.format(n)}"
+              case default =>
+                s"$default"
+            })
+        } else {
+          throw new NullPointerException(s"Null field value for field: ${field._1}")
         }
-        )
+      } else {
+        throw new NullPointerException("Null field name")
+      }
     }).mkString(",")
   }
 
